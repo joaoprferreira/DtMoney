ecaf71dad368c3b045ee744cbc0425ebc9e64dce
joaoprferreira <joaof082@gmail.com>
2021-10-31 04:07:10 -0300

    style: Criando e adicionando estilos globais na aplicação
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { GLobalStyle } from './styles/global';
+
 
 export function App() {
   return (
     <div className="App">
      <h1>Hello word</h1>
+     <GLobalStyle></GLobalStyle>
     </div>
   );
 }
