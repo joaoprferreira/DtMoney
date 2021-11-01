c6160fc6ea6ca7d37f3209aa1a6f407c741ef86c
joaoprferreira <joaof082@gmail.com>
2021-11-01 16:19:55 -0300

    Feat: Adcionando component Header na tela
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Header } from './components/Header';
 import { GLobalStyle } from './styles/global';
 
 
 export function App() {
   return (
     <div className="App">
+      <Header/>
      <h1>Hello word</h1>
      <GLobalStyle></GLobalStyle>
     </div>
