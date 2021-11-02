e783debd9d419b74554568606f9e2d7f0b62bb79
joaoprferreira <joaof082@gmail.com>
2021-11-02 01:52:56 -0300

    Feat: exportando closeImg
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,11 @@
 import incomeImg from "./income.svg"
 import outcomeImg from "./outcome.svg"
 import TotalImg from "./Total.svg"
+import CloseImg from "./close.svg"
 
 export {
   incomeImg,
   outcomeImg,
-  TotalImg
+  TotalImg,
+  CloseImg
 }
