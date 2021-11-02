4c2b2b953fec273dde6c5c84c4b5a5371913eba2
joaoprferreira <joaof082@gmail.com>
2021-11-01 22:04:12 -0300

    Feat: Criando component modal
    Signed-off-by: joaoprferreira <joaof082@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from "react-modal"
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GLobalStyle } from './styles/global';
 
+Modal.setAppElement("#root")
 
 export function App() {
+
+  const [isNewTrasactionModalOpen, setIsNewTrasactionModalOpen] = useState(false)
+
+  function handleOpenNewTrasanctionModal() {
+    setIsNewTrasactionModalOpen(true)
+  }
+
+  function handleCloseNewTrasanctionModal() {
+    setIsNewTrasactionModalOpen(false)
+  }
+
   return (
     <div className="App">
-      <Header/>
+      <Header onOpenNewTrasactionModal={handleOpenNewTrasanctionModal}/>
+
      <Dashboard/>
-     <GLobalStyle></GLobalStyle>
+
+     <Modal 
+        isOpen={isNewTrasactionModalOpen}
+        onRequestClose={handleCloseNewTrasanctionModal}
+        >
+          <h1>Cadastra transação</h1>
+        </Modal>
+
+     <GLobalStyle/>
     </div>
   );
 }
