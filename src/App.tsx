5b0fbb68cb2e07efa9bc247c26f6c6f4b1df6c7d
joao pedro <joao.ferreira@stormgroup.com.br>
2021-11-09 02:09:30 -0300

    Feat: colocando os dados direto no app
    Signed-off-by: joao pedro <joao.ferreira@stormgroup.com.br>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from "react-modal"
 import { ThemeProvider } from 'styled-components';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTrasactionModal } from './components/NewTrasactionModal';
+import { api } from './services/api';
 import { GLobalStyle } from './styles/global';
 import { theme } from './styles/theme';
+import { TransactionContext } from './TransactionContext';
 
 Modal.setAppElement("#root")
 
@@ -21,19 +23,38 @@ export function App() {
     setIsNewTrasactionModalOpen(false)
   }
 
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <Header onOpenNewTrasactionModal={handleOpenNewTrasanctionModal} />
-        <Dashboard />
-        <NewTrasactionModal
-          isOpen={isNewTrasactionModalOpen}
-          onRequestClose={handleCloseNewTrasanctionModal}
-        />
+  interface TrasnsactionProps {
+    id: string,
+    title: string,
+    amount: number,
+    category: string,
+    type: string,
+    createdAt: string
+  }
 
-        <GLobalStyle />
-      </div>
-    </ThemeProvider>
+
+  const [transactions, setTransactions] = useState<TrasnsactionProps[]>([])
+
+  useEffect(() => {
+    api.get('/transactions')
+      .then(response => setTransactions(response.data.transactions))
+  }, [])
+
+  return (
+    <TransactionContext.Provider value={transactions}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Header onOpenNewTrasactionModal={handleOpenNewTrasanctionModal} />
+          <Dashboard />
+          <NewTrasactionModal
+            isOpen={isNewTrasactionModalOpen}
+            onRequestClose={handleCloseNewTrasanctionModal}
+          />
+
+          <GLobalStyle />
+        </div>
+      </ThemeProvider>
+    </TransactionContext.Provider>
   );
 }
 
