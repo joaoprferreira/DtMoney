7f02fa08e736f0aa69c60d437aef22a6a7544609
joao pedro <joao.ferreira@stormgroup.com.br>
2021-11-11 23:18:52 -0300

    Feat: Criando contexto para as transactions
    Signed-off-by: joao pedro <joao.ferreira@stormgroup.com.br>

--- src/TransactionContext.tsx
+++ src/TransactionContext.tsx
@@ -1,3 +1,32 @@
-import { createContext } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "./services/api";
 
-export const TransactionContext = createContext([])
+export const TransactionContext = createContext<TrasnsactionProps[]>([])
+
+interface TrasnsactionProps {
+  id: string,
+  title: string,
+  amount: number,
+  category: string,
+  type: string,
+  createdAt: string,
+}
+
+interface TransactionProviderProps {
+  children: ReactNode
+} 
+
+export function TransactionProvider({children}: TransactionProviderProps){
+  const [transactions, setTransactions] = useState<TrasnsactionProps[]>([])
+
+  useEffect(() => {
+    api.get('/transactions')
+      .then(response => setTransactions(response.data.transactions))
+  }, [])
+
+  return (
+    <TransactionContext.Provider value={transactions}>
+      {children}
+    </TransactionContext.Provider>
+  )
+}
